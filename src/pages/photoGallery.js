f3c0cc3e80953e21e718dfb8666bccbdfd3f0bd9
Ashutosh Dash <dashashutosh1999@gmail.com>
2025-01-14 23:22:15 +0530

    feat: add images and image viewer

--- src/pages/photoGallery.js
+++ src/pages/photoGallery.js
@@ -1,64 +1,85 @@
 import Navbar from "components/navbar/Navbar";
 import Image from "next/image";
-import GalleryPic1 from "public/gallery/GalleryPic1.jpg";
-import GalleryPic2 from "public/gallery/GalleryPic2.jpg";
-import GalleryPic3 from "public/gallery/GalleryPic3.jpg";
-import GalleryPic4 from "public/gallery/GalleryPic4.jpg";
-import GalleryPic5 from "public/gallery/GalleryPic5.jpg";
-import GalleryPic6 from "public/gallery/GalleryPic6.jpg";
-import GalleryPic7 from "public/gallery/GalleryPic7.jpg";
-import GalleryPic8 from "public/gallery/GalleryPic8.jpg";
-import GalleryPic9 from "public/gallery/GalleryPic9.jpg";
-import GalleryPic10 from "public/gallery/GalleryPic10.jpg";
-import GalleryPic11 from "public/gallery/GalleryPic11.jpg";
-import GalleryPic12 from "public/gallery/GalleryPic12.jpg";
-import GalleryPic13 from "public/gallery/GalleryPic13.jpg";
-import GalleryPic14 from "public/gallery/GalleryPic14.jpg";
-import GalleryPic15 from "public/gallery/GalleryPic15.jpg";
-import GalleryPic16 from "public/gallery/GalleryPic16.jpg";
-import GalleryPic17 from "public/gallery/GalleryPic17.jpg";
+import { useCallback, useState } from "react";
+import ImageViewer from "react-simple-image-viewer";
 
 const galleryData = [
-  GalleryPic1,
-  GalleryPic2,
-  GalleryPic3,
-  GalleryPic4,
-  GalleryPic5,
-  GalleryPic6,
-  GalleryPic7,
-  GalleryPic8,
-  GalleryPic9,
-  GalleryPic10,
-  GalleryPic11,
-  GalleryPic12,
-  GalleryPic13,
-  GalleryPic14,
-  GalleryPic15,
-  GalleryPic16,
-  GalleryPic17,
+  "gallery/GalleryPic1.jpg",
+  "gallery/GalleryPic2.jpg",
+  "gallery/GalleryPic3.jpg",
+  "gallery/GalleryPic4.jpg",
+  "gallery/GalleryPic5.jpg",
+  "gallery/GalleryPic6.jpg",
+  "gallery/GalleryPic7.jpg",
+  "gallery/GalleryPic8.jpg",
+  "gallery/GalleryPic9.jpg",
+  "gallery/GalleryPic10.jpg",
+  "gallery/GalleryPic11.jpg",
+  "gallery/GalleryPic12.jpg",
+  "gallery/GalleryPic13.jpg",
+  "gallery/GalleryPic14.jpg",
+  "gallery/GalleryPic15.jpg",
+  "gallery/GalleryPic16.jpg",
+  "gallery/GalleryPic17.jpg",
+  "gallery/GalleryPic18.jpg",
+  "gallery/GalleryPic19.jpg",
+  "gallery/GalleryPic20.jpg",
+  "gallery/GalleryPic21.jpg",
+  "gallery/GalleryPic22.jpg",
+  "gallery/GalleryPic23.jpg",
+  "gallery/GalleryPic24.jpg",
+  "gallery/GalleryPic25.jpg",
+  "gallery/GalleryPic26.jpg",
+  "gallery/GalleryPic27.jpg",
 ];
 
 const PhotoGallery = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
   return (
     <div>
       <Navbar />
       <div className="container mx-auto max-w-[1200px] py-2 w-[95%]">
-        <h2 className="py-4 underline underline-offset-8 text-4xl text-center font-bold">ISTE Odisha Photo Gallery</h2>
+        <h2 className="py-4 underline underline-offset-8 text-4xl text-center font-bold">
+          ISTE Odisha Photo Gallery
+        </h2>
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {galleryData.map((item) => (
+          {galleryData?.map((item, index) => (
             <Image
-              key={item.src}
+              key={index}
               alt="ISTE Conference"
-              src={item.src}
+              src={item}
               width={200}
               height={200}
-              className="h-60 w-full rounded-xl object-cover"
-              unoptimized={true} 
-              placeholder="blur"
-              blurDataURL={item.blurDataURL}
+              className="h-60 w-full rounded-xl object-cover cursor-pointer"
+              unoptimized={true}
+              //   placeholder="blur"
+              //   blurDataURL={item.blurDataURL}
+              onClick={() => openImageViewer(index)}
             />
           ))}
         </div>
+
+        {isViewerOpen && (
+          <ImageViewer
+            src={galleryData}
+            currentIndex={currentImage}
+            disableScroll={false}
+            closeOnClickOutside={true}
+            onClose={closeImageViewer}
+          />
+        )}
       </div>
     </div>
   );
